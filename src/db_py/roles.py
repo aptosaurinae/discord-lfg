f60f354441bbc8fa0276fdf52a60295627f8946d
aptosaurinae <dukeslet@gmail.com>
2026-01-17 20:54:11 +0000

    remove rolespecific as no longer used

--- src/db_py/roles.py
+++ src/db_py/roles.py
@@ -13,16 +13,6 @@ class RoleType(Enum):
     dps = 3
 
 
-class RoleSpecific(Enum):
-    """Enumeration for the role a particular user has been assigned."""
-    none = 0
-    tank = 1
-    healer = 2
-    dps1 = 3
-    dps2 = 4
-    dps3 = 5
-
-
 @dataclass
 class Role:
     """Container for a particular role type."""
