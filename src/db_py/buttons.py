04d4fe748ce64dc30cf443059c41ec03f59894e0
aptosaurinae <dukeslet@gmail.com>
2026-01-17 21:06:03 +0000

    remove unused func

--- src/db_py/buttons.py
+++ src/db_py/buttons.py
@@ -5,23 +5,6 @@ import discord
 from db_py.db_instance import Role
 
 
-def _button(
-        custom_id: str,
-        emoji: str,
-        button_style: discord.ButtonStyle,
-        disabled: bool,
-        row: int,
-    ) -> discord.ui.Button:
-    button = discord.ui.Button(
-        style=button_style,
-        disabled=disabled,
-        custom_id=custom_id,
-        emoji=emoji,
-        row=row,
-    )
-    return button
-
-
 def button_from_role(role: Role, row: int) -> discord.ui.Button:
     """Creates a button from a given Role."""
     return discord.ui.Button(
